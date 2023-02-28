c768f7d6a129fe8ccd9e3c08ef41a539ccd92f82
William Sosropartono <wcsosropartono@gmail.com>
2023-02-28 12:19:48 -0800

    Basic layout for Home page added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-const lightMode = () => {
+
+const sections = document.querySelectorAll('.sections')
+const htmlSelector = document.querySelectorAll('html')
+
+const lightNightMode = document.querySelector('button')
+const nightMode = () => {
+    sections.forEach(section => 
+        section.classList.add("dark-mode"))
     
+}
+
+const lightMode = () => {
+    sections.forEach(section => {
+        section.classList.remove("darkmode")
+    })
 }
